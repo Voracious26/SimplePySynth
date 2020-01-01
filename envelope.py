9cc97f4423877acb8a9d1f718e3c2438d27df9d8
Voracious26 <37031448+Voracious26@users.noreply.github.com>
2020-01-01 12:51:33 -0500

    Update envelope.py

--- envelope.py
+++ envelope.py
@@ -65,15 +65,22 @@ class envelope:
                                 text="Release: ")
         
     def draw(self):
-        self.canvas.itemconfig(self.attackD, text="Attack: "+str(self.points[1].valueX))
-        self.canvas.itemconfig(self.decayD, text="Decay: "+str(self.points[2].valueX))
-        self.canvas.itemconfig(self.sustainD, text="Sustain: "+str(self.points[2].valueY))
-        self.canvas.itemconfig(self.releaseD, text="Release: "+str(self.points[4].valueX))
+
+        #self.canvas.itemconfig(self.attackD, text="Attack: "+str(self.points[1].valueX))
+        #self.canvas.itemconfig(self.decayD, text="Decay: "+str(self.points[2].valueX))
+        #self.canvas.itemconfig(self.sustainD, text="Sustain: "+str(self.points[2].valueY))
+        #self.canvas.itemconfig(self.releaseD, text="Release: "+str(self.points[4].valueX))
         
         self.currentA = ((self.points[1].valueX + 100)/200) * self.initialA
         self.currentD = ((self.points[2].valueX + 100)/200) * self.initialD
         self.currentS = ((self.points[2].valueY + 100)/200) * self.initialS
         self.currentR = ((self.points[4].valueX + 100)/200) * self.initialR
+
+        
+        self.canvas.itemconfig(self.attackD, text="Attack: "+str(round(self.currentA, 2)))
+        self.canvas.itemconfig(self.decayD, text="Decay: "+str(round(self.currentD, 2)))
+        self.canvas.itemconfig(self.sustainD, text="Sustain: "+str(round(self.currentS, 2)))
+        self.canvas.itemconfig(self.releaseD, text="Release: "+str(round(self.currentR, 2)))
         
         self.canvas.coords(self.lines[0], self.points[0].x, self.points[0].y, self.points[1].x, self.points[1].y)
         for d in range(1, len(self.points)-1):
