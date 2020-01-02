74d7d4ddf983853fb9a49903f0d94c7090ed30c2
Voracious26 <37031448+Voracious26@users.noreply.github.com>
2020-01-01 23:14:40 -0500

    Update SimplePySynth.py

--- SimplePySynth.py
+++ SimplePySynth.py
@@ -1,7 +1,9 @@
 import numpy as np
 import simpleaudio as sa
 from envelope import envelope
+from dial import dial
 from tkinter import Tk, Canvas
+import matplotlib.pyplot as plt
 import threading
 
 fs = 44100  # 44100 samples per second
@@ -27,7 +29,7 @@ def squareWave(frequency, length, n):
         outputWave += harmonic
     return outputWave
 
-def playNote(wavetype, frequency, seconds, attack, decay, sustain, release):
+def playNote(wavetype, frequency, seconds, volume, attack, decay, sustain, release):
     length = seconds + release
     
     A = attack * fs
@@ -42,15 +44,21 @@ def playNote(wavetype, frequency, seconds, attack, decay, sustain, release):
     elif wavetype == "sin":
         note = sinWave(frequency, length)
 
+    masterAmp = (volume/100)
     for i in range(0, note.size):
         amp = (i < A) * (i/A)
         amp += (A <= i < B) * (((sustain - 1)/(B-A))*(i - B) + sustain)
         amp += (B <= i < C) * sustain
         amp += (C <= i < D) * ((sustain/(C-D))*(i - D))
         note[i] *= amp
-    
+        note[i] *= masterAmp
+        if(note[i] > 1):
+            note[i] = 1
+        if(note[i] < -1):
+            note[i] = -1      
     # Ensure that highest value is in 16-bit range
-    audio = note * (2**15 - 1) / np.max(np.abs(note))
+    #audio = note * (2**15 - 1) / np.max(np.abs(note))
+    audio = note * (2**15 - 1)
     # Convert to 16-bit data
     audio = audio.astype(np.int16)
 
@@ -65,10 +73,12 @@ notes = [440, 880, 440, 220]
 root = Tk()
 paint = Canvas(root)  
 volumeEnvelope = envelope(paint)
+masterVolume = dial(paint, 350, 150, 20)
 
 def drawEverything():
     global volumeEnvelope
     volumeEnvelope.draw()
+    masterVolume.drawD()
 
 def key(event):
     repr(event.char)
@@ -77,16 +87,19 @@ def callback(event):
     global volumeEnvelope
     drawEverything()
     volumeEnvelope.clickedD(event)
+    masterVolume.clickedD(event)
 
 def moving(event):
     global volumeEnvelope
     drawEverything()
     volumeEnvelope.movingD(event)
+    masterVolume.movingD(event)
 
 def released(event):
     global volumeEnvelope
     drawEverything()
     volumeEnvelope.releasedD(event)
+    masterVolume.releasedD(event)
 
 paint.bind("<Key>", key)
 paint.bind("<Button-1>", callback)
@@ -103,8 +116,10 @@ def playNotes():
     for k in range(0, 3):
         for j in notes:
             #playNote("saw", j, 1, 0.5, 0.5, 1, 0.5)
-            playNote("saw", j, 1, volumeEnvelope.currentA, volumeEnvelope.currentD, volumeEnvelope.currentS, volumeEnvelope.currentR)
-            
+            #print(masterVolume.value/100)
+            playNote("saw", j, 1, masterVolume.value, volumeEnvelope.currentA, volumeEnvelope.currentD, 0.1, volumeEnvelope.currentR)
+
+
 while True:
     t2 = threading.Thread(target=playNotes, args=()) 
     t2.start()
