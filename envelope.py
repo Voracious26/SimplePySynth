fe26d4fb0c8d24038f15d5ceb2cbd035adf4dbbb
Voracious26 <37031448+Voracious26@users.noreply.github.com>
2020-01-02 13:46:43 -0500

    Update envelope.py

--- envelope.py
+++ envelope.py
@@ -1,4 +1,6 @@
 from point import point
+
+showText = False
 class envelope:
     def __init__(self, canvas):
         self.canvas = canvas
@@ -55,32 +57,29 @@ class envelope:
         for d in range(1, len(self.points)-1):
             self.lines.append(canvas.create_line(self.points[d].x, self.points[d].y, self.points[d+1].x, self.points[d+1].y))
 
-        self.attackD = canvas.create_text(300,10,fill="darkblue",font="Times 12",
-                                text="Attack: ")
-        self.decayD = canvas.create_text(300,25,fill="darkblue",font="Times 12",
-                                text="Decay: ")
-        self.sustainD = canvas.create_text(300,40,fill="darkblue",font="Times 12",
-                                text="Sustain: ")
-        self.releaseD = canvas.create_text(300,55,fill="darkblue",font="Times 12",
-                                text="Release: ")
+        global showText
+        if showText:
+            self.attackD = canvas.create_text(300,10,fill="darkblue",font="Times 12",
+                                    text="Attack: ")
+            self.decayD = canvas.create_text(300,25,fill="darkblue",font="Times 12",
+                                    text="Decay: ")
+            self.sustainD = canvas.create_text(300,40,fill="darkblue",font="Times 12",
+                                    text="Sustain: ")
+            self.releaseD = canvas.create_text(300,55,fill="darkblue",font="Times 12",
+                                    text="Release: ")
         
     def draw(self):
-
-        #self.canvas.itemconfig(self.attackD, text="Attack: "+str(self.points[1].valueX))
-        #self.canvas.itemconfig(self.decayD, text="Decay: "+str(self.points[2].valueX))
-        #self.canvas.itemconfig(self.sustainD, text="Sustain: "+str(self.points[2].valueY))
-        #self.canvas.itemconfig(self.releaseD, text="Release: "+str(self.points[4].valueX))
-        
+    
         self.currentA = ((self.points[1].valueX + 100)/200) * self.initialA
         self.currentD = ((self.points[2].valueX + 100)/200) * self.initialD
         self.currentS = ((self.points[2].valueY + 100)/200) * self.initialS
         self.currentR = ((self.points[4].valueX + 100)/200) * self.initialR
-
-        
-        self.canvas.itemconfig(self.attackD, text="Attack: "+str(round(self.currentA, 2)))
-        self.canvas.itemconfig(self.decayD, text="Decay: "+str(round(self.currentD, 2)))
-        self.canvas.itemconfig(self.sustainD, text="Sustain: "+str(round(self.currentS, 2)))
-        self.canvas.itemconfig(self.releaseD, text="Release: "+str(round(self.currentR, 2)))
+        global showText
+        if showText:
+            self.canvas.itemconfig(self.attackD, text="Attack: "+str(round(self.currentA, 2)))
+            self.canvas.itemconfig(self.decayD, text="Decay: "+str(round(self.currentD, 2)))
+            self.canvas.itemconfig(self.sustainD, text="Sustain: "+str(round(self.currentS, 2)))
+            self.canvas.itemconfig(self.releaseD, text="Release: "+str(round(self.currentR, 2)))
         
         self.canvas.coords(self.lines[0], self.points[0].x, self.points[0].y, self.points[1].x, self.points[1].y)
         for d in range(1, len(self.points)-1):
